488641a768c60f353cf8a9d55f9bceda15837943
Jadson Sanches <jadson.sanches@7comm.com.br>
2021-08-18 14:26:00 -0300

    Added constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const primaryColorLightTheme = Color(0xFF3AC072);
+const secondaryColorLightTheme = Color(0xFF32FB567);
+
+const primaryColorDarkTheme = Color(0xFF282B33);
+const secondaryColorDarkTheme = Color(0xFF2E343D);
+
+const warninngColor = Color(0xFFF3BB1C);
+const errorColor = Color(0xFFF03738);
+
+const contentColorLightTheme = Color(0xFF3AC072);
+const contentColorDarkTheme = Color(0xFF282B33);
+
+const textColor = Color(0xFF535353);
+// const kTextLightColor = Color(0xFFACACAC);
+
+const defaultPadding = 20.0;
